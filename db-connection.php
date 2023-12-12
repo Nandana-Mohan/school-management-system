94bbc6a8a75a1da08bf7f7821d0298d4549cc364
Nandana-Mohan <115038219+Nandana-Mohan@users.noreply.github.com>
2023-12-12 10:29:46 +0530

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -2,7 +2,7 @@
 define("MYSQL_SERVER", "sysmsql.mysql.database.azure.com");
 define("MYSQL_USER", "sysmsql");
 define("MYSQL_PASSWORD", "March14032003");
-define("MYSQL_DATABASE", "mebis");
+define("MYSQL_DATABASE", "oopse");
 
 $mysqli = new mysqli(MYSQL_SERVER,MYSQL_USER,MYSQL_PASSWORD) or die ('I cannot connect to the database because 1: ' . $mysqli->error);
 
